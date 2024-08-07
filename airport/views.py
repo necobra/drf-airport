eac0722841e06c2651f374d45af9e928df4d113e
necobra <necobrane105@gmail.com>
2024-08-07 13:45:23 +0300

    refactor viewsets

--- airport/views.py
+++ airport/views.py
@@ -1,59 +1,44 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from airport import models
+from airport import models, serializers
 
 
 class AirportViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Airport
-        fields = ("id", "name", "closest_big_city")
+    queryset = models.Airport.objects.all()
+    serializer_class = serializers.AirportSerializer
 
 
 class RouteViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Route
-        fields = ("id", "source", "destination", "distance")
+    queryset = models.Route.objects.all()
+    serializer_class = serializers.RouteSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Crew
-        fields = ("id", "first_name", "last_name")
+    queryset = models.Crew.objects.all()
+    serializer_class = serializers.CrewSerializer
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.AirplaneType
-        fields = ("id", "name")
+    queryset = models.AirplaneType.objects.all()
+    serializer_class = serializers.AirplaneTypeSerializer
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Airplane
-        fields = (
-            "id", "rows", "seats_in_row", "airplane_type", "capacity"
-        )
+    queryset = models.Airplane.objects.all()
+    serializer_class = serializers.AirplaneSerializer
+
 
 class FlightViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Flight
-        fields = (
-            "id", "route", "airplane", "departure_time", "arrival_time", "crew"
-        )
+    queryset = models.Flight.objects.all()
+    serializer_class = serializers.FlightSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Ticket
-        fields = (
-            "row", "seat", "flight"
-        )
+    queryset = models.Ticket.objects.all()
+    serializer_class = serializers.TicketSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    class Meta:
-        model = models.Order
-        fields = (
-            "created_at", "user"
-        )
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
