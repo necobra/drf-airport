4e24e622c1ca26154f64361b9c0f86d84198ef4a
necobra <necobrane105@gmail.com>
2024-08-07 12:22:31 +0300

    add urls for airport api

---
+++ airport/urls.py
@@ -0,0 +1,18 @@
+from rest_framework.routers import DefaultRouter
+
+from airport import views
+
+
+router = DefaultRouter()
+router.register("orders", views.OrderViewSet)
+router.register("crew", views.CrewViewSet)
+router.register("airplane_types", views.AirplaneTypeViewSet)
+router.register("airplanes", views.AirplaneViewSet)
+router.register("airports", views.AirportViewSet)
+router.register("flights", views.FlightViewSet)
+router.register("tickets", views.TicketViewSet)
+
+
+urlpatterns = router.urls
+
+app_name = "airport"
